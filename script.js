c7a07d809e85eab4e756fccf3c5a3cdaec8acbcb
lambertwijshakecreations <73013216+lambertwijshakecreations@users.noreply.github.com>
2021-01-21 19:43:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//Dit is deel 1 van de opdracht.
+/*const age = 18;     //vaste leeftijd is 18jaar
+
+if (age == 18){
+    console.log("Welkom!");     // Als iemand 18 jaar is mag die naar binnen.
+}else if (age > 18){
+    console.log("Welkom!");     // Als iemand ouder is dan 18 jaar mag die naar binnen.
+}else{
+    console.log("Sorry je bent te jong om binnen te mogen komen.");     //Als iemand jonger is dan 18 jaar mag die niet naar binnen.
+}*/
+
+var age = 17;
+
+if (age == 18){
+    console.log("Welkom!");     // Als iemand 18 jaar is mag die naar binnen.
+}else if (age > 18){
+    console.log("Welkom!");     // Als iemand ouder is dan 18 jaar mag die naar binnen.
+}else{
+    console.log("Sorry je bent te jong om binnen te mogen komen.");     //Als iemand jonger is dan 18 jaar mag die niet naar binnen.
+}
+
+// Dit is deel 2 van de opdracht.
+const isFemale = true;      //De persoon is een vrouw
+if (isFemale) {
+    console.log("Hello lady");       //Als de persoon een vrouw is, kom binnen.
+}else {
+    console.log("no lady, no welkom.");     //Als de persoon gee vrouw is mag die niet naar binnen.
+}
+
+//Dit is deel 3 van de opdracht.
+const driverStatus = "Bob";     //status die gelijk staat aan nuchter/Bob.
+if (driverStatus) {
+    console.log("U mag rijden");    //Als je nuchter bent mag je rijden.
+}else{
+    console.log("U mag niet rijden"); //Als je dronken bent mag je niet rijden.
+}
+
+
+
+
